06ec34385d465fd74334d14d1b43b15e50720b1c
dekah2077 <andressargentinoo@gmail.com>
2025-03-29 14:11:42 -0300

    Update

--- sam_logs/lua/sam_discord_integration/sv_discord.lua
+++ sam_logs/lua/sam_discord_integration/sv_discord.lua
@@ -5,13 +5,6 @@ local Config = SAM_DISCORD_INTEGRATION.Config
 local Lang = Config.Languages[Config.Language] or Config.Languages.en
 local Settings = Config.Settings
 
-<<<<<<< HEAD
--- Sends punishment data to Discord as an embed
-local function SendPunishmentEmbed(target_name, staff_name, sanction_type, duration, steamid, reason)
-    local steamid64 = (steamid ~= "Not found." and util.SteamIDTo64(steamid)) or "76561197960265728"
-    local color = Config.Colors[sanction_type] or 0xFFFFFF
-    local duration_text = (sanction_type == "Kick" and "N/A") or (duration == 0 and "Permanent") or sam.format_length(duration)
-=======
 local function SendPunishmentEmbed(target_name, staff_name, sanction_type, duration, steamid, reason)
     local steamid64 = (steamid ~= "Not found." and util.SteamIDTo64(steamid)) or "76561197960265728"
     local color = Config.Colors[sanction_type] or 0xFFFFFF
@@ -41,7 +34,6 @@ local function SendPunishmentEmbed(target_name, staff_name, sanction_type, durat
             duration_text = seconds .. " " .. (seconds == 1 and Lang.Second or Lang.Seconds)
         end
     end
->>>>>>> master
 
     local fields = {}
     if Settings.ShowPlayer then table.insert(fields, { name = Lang.Player, value = "`" .. target_name .. "`", inline = true }) end
